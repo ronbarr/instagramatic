b442c4b6c23e6970fcf92e05827bb4beb27e2dda
ronbarr <ron.barr@icloud.com>
2014-10-25 18:26:55 -0700

    Initial Commit
    Signed-off-by: ronbarr <ron.barr@icloud.com>

--- Instagramatic/InstaViewController.h
+++ Instagramatic/InstaViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  InstaViewController.h
 //  Instagramatic
 //
 //  Created by Ron Barr on 10/25/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface InstaViewController : UICollectionViewController
 
 @end
-
